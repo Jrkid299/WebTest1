f1cfc7cdd3532d5a3f07114733fa4f5053d08107
Jalen Lamb <2018118881@ub.edu.bz>
2022-11-12 21:09:11 -0600

    server and database installed

---
+++ cmd/api/main.go
@@ -0,0 +1,11 @@
+  
+            // File: cmd/api/main.go
+
+            package main
+
+            import "fmt"
+
+            func main() {
+                fmt.Println("Hello world!")
+            } 
+       
